86cff53d6a8add5131da604f2a9976e1a6db3722
Jeevan <jeevanc162@gmail.com>
2021-10-29 17:44:31 +0530

    Edit and Delete an User

--- src/container/users/ListUser.js
+++ src/container/users/ListUser.js
@@ -13,21 +13,29 @@ const ListUser = (props) => {
                         <th scope="col">Name</th>
                         <th scope="col">Email</th>
                         <th scope="col">Date Of Birth</th>
+                        <th scope="col">Delete</th>
+                        <th scope="col">Edit</th>
                     </tr>
                 </thead>
                 <tbody>
-                {props.data.map((user, index) => {
+                    {props.data.map((user, index) => {
                         let temp = ++count
                         return (
                             <tr key={temp}>
-                            <td>{temp}</td>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
-                            <td>{user.dob}</td>
+                                <td>{temp}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.dob}</td>
+                                <td>
+                                    <button onClick={() => props.deleteRow(index)}>Delete</button>
+                                </td>
+                                <td>
+                                    <button onClick={() => props.editRow(index)}>Edit</button>
+                                </td>
                             </tr>
                         )
-                        })}
-                 
+                    })}
+
                 </tbody>
             </table>
         </div>
